4213a6eb67198561439aeb3c3b71ef1346b840b8
lenyagolikov <jayhosee@gmail.com>
2021-12-23 00:48:31 +0300

    third solution for missing numbers (448.py)

--- arrays/448.py
+++ arrays/448.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
-def missing_numbers(nums: list) -> list:
+
+def missing_numbers(nums: list[int]) -> list[int]:
     """Return an array of all the integers in the range [1, n] that do not appear in nums
 
     nums: list[int]
@@ -17,7 +18,7 @@ def missing_numbers(nums: list) -> list:
     return result
 
 
-def missing_numbers(nums: list) -> list:
+def missing_numbers(nums: list[int]) -> list[int]:
     """Return an array of all the integers in the range [1, n] that do not appear in nums
     Without extra memory (cyclic sort)
 
@@ -35,6 +36,28 @@ def missing_numbers(nums: list) -> list:
     result = []
     for i in range(len(nums)):
         if nums[i] != i + 1:
+            result.append(i + 1)
+
+    return result
+
+
+def missing_numbers(nums: list[int]) -> list[int]:
+    """
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    >>> missing_numbers([4, 3, 2, 7, 8, 2, 3, 1])
+    [5, 6]
+    >>> missing_numbers([1, 1])
+    [2]
+    """
+    result = []
+
+    for num in nums:
+        if nums[abs(num) - 1] > 0:
+            nums[abs(num) - 1] *= -1
+
+    for i in range(len(nums)):
+        if nums[i] > 0:
             result.append(i + 1)
 
     return result
